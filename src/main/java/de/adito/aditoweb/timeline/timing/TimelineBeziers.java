2d74a6df1439977eafd91999ed5930f689271a21
k.mifka <k.mifka@adito.de>
2018-06-22 08:44:13 +0200

    Standard-Timeline-Beziers ergänzt

--- src/main/java/de/adito/aditoweb/timeline/timing/TimelineBeziers.java
+++ src/main/java/de/adito/aditoweb/timeline/timing/TimelineBeziers.java
@@ -8,6 +8,8 @@ package de.adito.aditoweb.timeline.timing;
 public final class TimelineBeziers
 {
   public static final ITimelineBezier LINEAR = new TimelineBezier(0, 0, 1, 1);
-
-  public static final ITimelineBezier EASE_OUT = new TimelineBezier(0, 1, 0, 1);
+  public static final ITimelineBezier EASE = new TimelineBezier(.25F,.1F,.25F,1);
+  public static final ITimelineBezier EASE_IN = new TimelineBezier(.42F,0,1,1);
+  public static final ITimelineBezier EASE_OUT = new TimelineBezier(0,0,.58F,1);
+  public static final ITimelineBezier EASE_IN_OUT = new TimelineBezier(.42F,0,.58F,1);
 }
